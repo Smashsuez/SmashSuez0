274a92d82b0f1eaa57b775137a6d064fc2bd4f59
Smashsuez <mohamedahmedgameel@gmail.com>
2023-05-19 20:04:49 +0300

    ad

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '/styles/Home.module.css'
 import Landing from '@/components/Landinglatest';
 import List from '@/components/Listlatest';
 import Location from '@/components/Locationlatest';
 import axios from "axios";
-import orderDetail from "@/components/OrderDetaillatest"
 import { useState } from 'react';
 import Add from '@/components/Addlatest';
 import AddButton from '@/components/AddButtonlatest';
