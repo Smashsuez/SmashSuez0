3e737fd6291c12c01e4fd239df09d824a99c70c7
Smashsuez <mohamedahmedgameel@gmail.com>
2023-05-20 09:46:46 +0300

    da

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import { Inter } from 'next/font/google'
 import Landing from '@/components/Landinglatest';
 import List from '@/components/Listlatest';
 import Location from '@/components/Locationlatest';
-import OrderDetail from '@/components/OrderDetaillatest';
 import axios from "axios";
 import { useState } from 'react';
 import Add from '@/components/Addlatest';
