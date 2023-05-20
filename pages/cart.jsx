a7c3d7cd5ccd738d6774cd238331b9ba4669297d
Smashsuez <mohamedahmedgameel@gmail.com>
2023-05-21 01:04:36 +0300

    fsd

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -4,26 +4,28 @@ import Image from 'next/legacy/image';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeProduct, updateCart, reset } from '../redux/cartSlice';
 import axios from 'axios';
-import { useRouter } from "next/router";
-import OrderDetail from "../components/OrderDetail";
-import Head from 'next/head'
+import { useRouter } from 'next/router';
+import OrderDetail from '../components/OrderDetail';
+import Head from 'next/head';
 
-
-const Cart = () =>{
+const Cart = () => {
   const cart = useSelector((state) => state.cart);
   const [open, setOpen] = useState(true);
   const [cash, setCash] = useState(false);
   const dispatch = useDispatch();
   const router = useRouter();
 
-
   const createOrder = async (data) => {
     try {
-      const res = await axios.post('https://smash-suez0-todb.vercel.app/api/orders', data);
+      const res = await axios.post(
+        'https://cors-anywhere.herokuapp.com/https://smash-suez0.vercel.app/api/orders',
+        data,
+        { headers: { 'X-Requested-With': 'XMLHttpRequest' } }
+      );
       if (res.status === 201) {
         dispatch(reset());
         router.push(`/orders/${res.data._id}`);
-        console.log(data)
+        console.log(data);
       }
     } catch (err) {
       console.log(err);
@@ -34,9 +36,7 @@ const Cart = () =>{
     dispatch(removeProduct(productId));
   };
 
-  
   return (
-    
     <div className={styles.container}>
       <Head>
         <title>Smash Burger</title>
@@ -44,7 +44,7 @@ const Cart = () =>{
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/imgs/ico.png" />
         <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"></link>
-        <meta charSet='utf-8' />
+        <meta charSet="utf-8" />
       </Head>
       <div className={styles.left}>
         <table className={styles.table}>
@@ -63,16 +63,13 @@ const Cart = () =>{
               <tr className={styles.tr} key={product._id}>
                 <td className={styles.imgTd}>
                   <div className={styles.imgContainer}>
-                    <Image
-                      src={product.img}
-                      layout="fill"
-                      objectFit="cover"
-                      alt=""
-                    />
+                    <Image src={product.img} layout="fill" objectFit="cover" alt="" />
                   </div>
                 </td>
                 <td>
-                  <span className={styles.name}>{product.size.text} {product.title}</span>
+                  <span className={styles.name}>
+                    {product.size.text} {product.title}
+                  </span>
                 </td>
                 <td>
                   <span className={styles.extras}>
@@ -88,13 +85,11 @@ const Cart = () =>{
                   <span className={styles.quantity}>{product.quantity}</span>
                 </td>
                 <td>
-                  <span className={styles.total}>
-                  £ {product.price * product.quantity}
-                  </span>
+                  <span className={styles.total}>£ {product.total}</span>
                 </td>
                 <td>
                   <button
-                    className={styles.removeButton}
+                    className={styles.removeBtn}
                     onClick={() => handleRemove(product._id)}
                   >
                     Remove
@@ -106,38 +101,52 @@ const Cart = () =>{
         </table>
       </div>
       <div className={styles.right}>
-        <div className={styles.wrapper}>
-          <h2 className={styles.title}>CART TOTAL</h2>
-          <div className={styles.totalText}>
-            <b className={styles.totalTextTitle}>Total:</b>£ {cart.total}
+        <div className={styles.orderSummary}>
+          <div className={styles.title}>
+            <h2>Order Summary</h2>
           </div>
-          {open ? (
-            <div className={styles.paymentMethods}>
-              <button
-                className={styles.button}
-                style={{ width: "100%" }}
-                onClick={() => setCash(true)}
-              >
-              CASH ON DELIVERY
-              </button>
-
-                </div>
-              ) : (
-                <button onClick={() => setOpen(true)} className={styles.button}>
-              CHECKOUT NOW!
-              </button>
-                  )}
-                </div>
-              </div>
-      
-              {cash && <OrderDetail total={cart.total} setCash={setCash} createOrder={createOrder} quantity={cart.products.map((product) => (product.quantity))} extra={cart.products.map((product) => (product.extras.map((extra) => (
-                     extra.text
-                    ))))} title={cart.products.map((product) => (product.title))} size={cart.products.map((product) => (product.size.text))} />}
-
-
-              </div>
-              
-
+          <div className={styles.subTotal}>
+            <h3>Subtotal</h3>
+            <span>£ {cart.subTotal}</span>
+          </div>
+          <div className={styles.total}>
+            <h3>Total</h3>
+            <span>£ {cart.total}</span>
+          </div>
+          <div className={styles.buttons}>
+            <button
+              className={styles.cancelBtn}
+              onClick={() => {
+                setOpen(false);
+                dispatch(reset());
+              }}
+            >
+              Cancel
+            </button>
+            <button
+              className={styles.confirmBtn}
+              onClick={() => {
+                setOpen(false);
+                setCash(true);
+              }}
+            >
+              Confirm
+            </button>
+          </div>
+        </div>
+        {open ? (
+          ''
+        ) : (
+          <OrderDetail
+            cash={cash}
+            cart={cart}
+            createOrder={createOrder}
+            setCash={setCash}
+            setOpen={setOpen}
+          />
+        )}
+      </div>
+    </div>
   );
 };
 
