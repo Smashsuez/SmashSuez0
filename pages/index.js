85ab52d99a04ac3ccd4e16716d0937164a82dbe2
Smashsuez <mohamedahmedgameel@gmail.com>
2023-05-20 20:42:52 +0300

    kj

--- pages/index.js
+++ pages/index.js
@@ -48,7 +48,7 @@ export const getServerSideProps = async (ctx) => {
 
 
 
-  const res = await axios.get("http://localhost:3000/api/products");
+  const res = await axios.get("https://smash-suez0.vercel.app/api/products");
   return{
     props:{
       list: res.data,
