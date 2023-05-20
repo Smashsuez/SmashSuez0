89334755a78578733de6e28c9cc48c333eda7aaf
Smashsuez <mohamedahmedgameel@gmail.com>
2023-05-20 09:29:18 +0300

    c

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -5,7 +5,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { removeProduct, updateCart, reset } from '../redux/cartSlice';
 import axios from 'axios';
 import { useRouter } from "next/router";
-import OrderDetail from '@/components/OrderDetaillatest';
+import OrderDetail from '../components/OrderDetaillatest';
 import Head from 'next/head'
 
 
