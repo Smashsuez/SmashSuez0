d7fe092029cc60167f0eaa361c14571dd557a12b
Smashsuez <mohamedahmedgameel@gmail.com>
2023-06-14 23:10:05 +0300

    footer back

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div className={styles.container}>
       <div className={styles.item}>ORDER NOW: 010 2002 7359</div>
-      <div className={`${styles.item} ${styles.me}`}>Powered By M.Gameel. 0111 38 32 120 <span className={styles.small}>WhatsApp Only</span></div>
+      <div className={`${styles.item} ${styles.me}`}>This Website Made by Mohamed Gameel. 0111 38 32 120 <span className={styles.small}>WhatsApp Only</span></div>
     </div>
   )
 }
