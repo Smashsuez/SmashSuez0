3849851ef4f70bff93248a70b18fb58e5f6550c4
Smashsuez <mohamedahmedgameel@gmail.com>
2023-07-04 12:28:16 +0300

    new nav

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,8 @@
-import React, { Fragment ,useState, useEffect } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import styles from "../styles/Navbar.module.css";
 import Image from 'next/image';
 import { useSelector } from "react-redux";
 import Link from "next/link";
-// import SmoothScroll from "smooth-scroll";
-
 
 const Navbar = () => {
   const quantity = useSelector((state) => state.cart.quantity);
@@ -29,58 +27,49 @@ const Navbar = () => {
   const handleScrollTo = (id) => {
     const element = document.getElementById(id);
     if (element) {
-      
-        const element = document.getElementById(id);
-        
-          element.scrollIntoView({ behavior: "smooth" });
-        
-      
-    }else{
-      // window.href = "/";
-        if (element) {
-          element.scrollIntoView({ behavior: "smooth" });
-        }
-  };}
-  
-  
-
-  
-
-//   const scroll = new SmoothScroll('a[href*="#"]', {
-//     speed: 300
-// });
+      element.scrollIntoView({ behavior: "smooth" });
+    } else {
+      // If the element is not found, redirect to the homepage
+      window.location.href = "/";
+    }
+  };
 
   return (
-    <Fragment >
-    
+    <Fragment>
       <div className={transitionNavbar ? `${styles.container} ${styles.active}` : styles.container}>
-      <Link href="/" passHref>
-        <div className={styles.logo}>
-          <Image src="/imgs/WhatsApp Image 2023-06-12 azzzzzzzzzzzzzzzzzzzzzzzt 22.50.49.png" width="120" height="90" alt=""/>
-        </div></Link>
+        <Link href="/" passHref>
+          <div className={styles.logo}>
+            <Image src="/imgs/WhatsApp Image 2023-06-12 azzzzzzzzzzzzzzzzzzzzzzzt 22.50.49.png" width="120" height="90" alt="" />
+          </div>
+        </Link>
         <div className={styles.item}>
           <div className={styles.mid}>
-          <ul className={styles.list}>
-            <Link href="/" passHref>
-            <li className={styles.listItem}>Homepage</li></Link>
-            <Link href="/" passHref><li className={styles.listItem} onClick={() => handleScrollTo("menu")}>Menu</li></Link>
-            
-            <Link href="/" passHref><li className={styles.listItem} onClick={() => handleScrollTo("location")}>Location</li></Link>
-            
-          </ul></div>
-        </div>
-        <Link href="/cart" passHref>
-        <div className={styles.item}>
-        <div className={styles.cart}>
-            <Image src="/imgs/cart.png" width="20" height="20" alt=""/>
-            <div className={styles.counter}>{quantity}</div>
+            <ul className={styles.list}>
+              <li className={styles.listItem}>
+                <Link href="/" passHref>
+                  <a>Homepage</a>
+                </Link>
+              </li>
+              <li className={styles.listItem}>
+                <a onClick={() => handleScrollTo("menu")}>Menu</a>
+              </li>
+              <li className={styles.listItem}>
+                <a onClick={() => handleScrollTo("location")}>Location</a>
+              </li>
+            </ul>
           </div>
         </div>
+        <Link href="/cart" passHref>
+          <div className={styles.item}>
+            <div className={styles.cart}>
+              <Image src="/imgs/cart.png" width="20" height="20" alt="" />
+              <div className={styles.counter}>{quantity}</div>
+            </div>
+          </div>
         </Link>
       </div>
-      
-      </Fragment>
-    
+    </Fragment>
   );
 };
-export default Navbar
+
+export default Navbar;
