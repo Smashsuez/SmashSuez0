c0ae46f36647c5189b7efac67283b3a706f5a19e
Smashsuez <mohamedahmedgameel@gmail.com>
2023-07-04 11:15:29 +0300

    extra logic 2

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -71,6 +71,7 @@ const Product = ({ burger }) => {
       // remove the current option from extras and deduct its price from the total
       setExtras(extras.filter((extra) => extra._id !== option._id));
       setPrice(price - option.price);
+      setExtras((prev) => [...prev, option]);
     }
   };
   
