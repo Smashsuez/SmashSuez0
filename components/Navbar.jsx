33dfe11fac85bfbe67e9302943705cb21a1313ac
Smashsuez <mohamedahmedgameel@gmail.com>
2023-07-08 17:57:13 +0300

    nav

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -68,7 +68,7 @@ const toggleNavActivee = () => {
   return (
     <Fragment >
     
-      <div className={`${styles.container} ${transitionNavbar ? styles.activeBar : ''}`}>
+      <div className={ styles.container}>
       <Link href="/" passHref>
         <div className={styles.logo}>
           <Image src="/imgs/WhatsApp Image 2023-06-12 azzzzzzzzzzzzzzzzzzzzzzzt 22.50.49.png" width="120" height="90" alt=""/>
