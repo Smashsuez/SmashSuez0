531c1bd3c4ef873ef43ca6b21f849477f50a88fb
Smashsuez <mohamedahmedgameel@gmail.com>
2023-07-08 11:45:49 +0300

    new nav

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,8 @@
-import React, { Fragment ,useState, useEffect } from "react";
-import styles from "../styles/Navbar.module.css";
+import React, { Fragment, useState, useEffect } from 'react';
+import styles from '../styles/Navbar.module.css';
 import Image from 'next/image';
-import { useSelector } from "react-redux";
-import Link from "next/link";
-// import SmoothScroll from "smooth-scroll";
-
+import { useSelector } from 'react-redux';
+import Link from 'next/link';
 
 const Navbar = () => {
   const quantity = useSelector((state) => state.cart.quantity);
@@ -20,77 +18,67 @@ const Navbar = () => {
   };
 
   useEffect(() => {
-    window.addEventListener("scroll", changeBackground);
+    window.addEventListener('scroll', changeBackground);
     return () => {
-      window.removeEventListener("scroll", changeBackground);
+      window.removeEventListener('scroll', changeBackground);
     };
   }, []);
 
- const handleScrollTo = (id) => {
-  const element = document.getElementById(id);
-  if (element) {
-    setTimeout(() => {
-      element.scrollIntoView({ behavior: "smooth" });
-    }, 1000); // Wait for 1 second before scrolling to the component
-  } else {
-    window.location.href = "/";
-    // setTimeout(() => {
-    //   const element = document.getElementById(id);
-    //   if (element) {
-    //     element.scrollIntoView({ behavior: "smooth" });
-    //   }
-    // }, 4000);
+  const sectionExists = (id) => {
     const element = document.getElementById(id);
-  if (element) {
-    setTimeout(() => {
-      element.scrollIntoView({ behavior: "smooth" });
-    }, 4000);}
-     // Wait for 2 seconds before redirecting and attempting to scroll to the component
-  }
-};
+    return !!element; // Returns true if element exists, false otherwise
+  };
 
-  
-  
-
-  
-
-//   const scroll = new SmoothScroll('a[href*="#"]', {
-//     speed: 300
-// });
+  const handleScrollTo = (id) => {
+    if (sectionExists(id)) {
+      const element = document.getElementById(id);
+      element.scrollIntoView({ behavior: 'smooth' });
+    } else {
+      window.location.href = '/';
+      // Redirect to homepage and attempt scrolling after a delay
+      setTimeout(() => {
+        const element = document.getElementById(id);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth' });
+        }
+      }, 4000);
+    }
+  };
 
   return (
-    <Fragment >
-    
-      <div className={transitionNavbar ? `${styles.container} ${styles.active}` : styles.container}>
-      <Link href="/" passHref>
-        <div className={styles.logo}>
-          <Image src="/imgs/WhatsApp Image 2023-06-12 azzzzzzzzzzzzzzzzzzzzzzzt 22.50.49.png" width="120" height="90" alt=""/>
-        </div></Link>
+    <Fragment>
+      <div className={`${styles.container} ${transitionNavbar ? styles.active : ''}`}>
+        <Link href="/" passHref>
+          <div className={styles.logo}>
+            <Image src="/imgs/WhatsApp Image 2023-06-12 azzzzzzzzzzzzzzzzzzzzzzzt 22.50.49.png" width="120" height="90" alt="" />
+          </div>
+        </Link>
         <div className={styles.item}>
           <div className={styles.mid}>
-          <ul className={styles.list}>
-            <Link href="/" passHref>
-            <li className={styles.listItem}>Homepage</li></Link>
-            <Link href="/" passHref><li className={styles.listItem} onClick={() => handleScrollTo("menu")}>Menu</li></Link>
-            
-            <Link href="/" passHref><li className={styles.listItem} onClick={() => handleScrollTo("location")}>Location</li></Link>
-            
-          </ul></div>
-        </div>
-        <Link href="/cart" passHref>
-        <div className={styles.item}>
-        <div className={styles.cart}>
-            <Image src="/imgs/cart.png" width="20" height="20" alt=""/>
-            <div className={styles.counter}>{quantity}</div>
+            <ul className={styles.list}>
+              <Link href="/" passHref>
+                <li className={styles.listItem}>Homepage</li>
+              </Link>
+              <li className={styles.listItem} onClick={() => handleScrollTo('menu')}>
+                Menu
+              </li>
+              <li className={styles.listItem} onClick={() => handleScrollTo('location')}>
+                Location
+              </li>
+            </ul>
           </div>
         </div>
+        <Link href="/cart" passHref>
+          <div className={styles.item}>
+            <div className={styles.cart}>
+              <Image src="/imgs/cart.png" width="20" height="20" alt="" />
+              <div className={styles.counter}>{quantity}</div>
+            </div>
+          </div>
         </Link>
       </div>
-      
-      </Fragment>
-    
+    </Fragment>
   );
 };
 
 export default Navbar;
-
